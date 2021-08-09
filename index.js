c3d76f2b4d8131eeb9464518b7d4ed9c497c7aae
Wiifek <chabir.wifek@gmail.com>
2021-08-09 14:20:11 +0100

    changed port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const morgan = require('morgan')
 const dotenv = require("dotenv");
 const cors = require('cors') 
 const app = express()
-const port = 3000
+const port = 4000
 
 // body parser json 
 app.use(express.json())
